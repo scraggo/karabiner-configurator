0f6d3601cf5ba76adb036b2226b6adb96057dbc2
Dave Cohen <scraggo@gmail.com>
2023-07-30 22:55:18 -0500

    Update

--- my-config/mouse.ts
+++ my-config/mouse.ts
@@ -1,11 +1,36 @@
 import { FromKeyCode, map, ToMouseKey } from 'karabiner.ts';
 
-const createMouseMovement = (
+/** [x, y] */
+type Multiplier = -1 | 1 | null;
+type Multipliers = [Multiplier, Multiplier];
+
+const DISTANCES = {
+  long: 2000,
+  med: 1200,
+  short: 600,
+};
+
+const DIRECTIONS: Record<string, [Multiplier, Multiplier]> = {
+  '↑': [null, -1],
+  '↖': [-1, -1],
+  '↗': [1, -1],
+  '↓': [null, 1],
+  '↙': [-1, 1],
+  '↘': [1, 1],
+  '←': [-1, null],
+  '→': [1, null],
+};
+
+const moveBy = (multipliers: Multipliers, distance: number) =>
+  multipliers.map((m) => (m ? m * distance : null));
+
+const makeMouseKey = (
   from: FromKeyCode,
-  x?: number | null,
-  y?: number | null
+  multipliers: Multipliers,
+  distance: number
 ) => {
   const to = {} as ToMouseKey;
+  const [x, y] = moveBy(multipliers, distance);
 
   if (typeof x === 'number') {
     to.x = x;
@@ -30,7 +55,7 @@ const createMouseMovement = (
  *   }
  * },
  */
-const createScroll = (
+const makeMouseScroll = (
   from: FromKeyCode,
   x?: number | null,
   y?: number | null
@@ -53,62 +78,15 @@ const createScroll = (
   return map(from).toMouseKey(to);
 };
 
-const DISTANCES = {
-  long: 2000,
-  med: 1200,
-  short: 600,
-};
-
-const UP = {
-  fast: createMouseMovement('i', null, -DISTANCES.long),
-  med: createMouseMovement('i', null, -DISTANCES.med),
-  slow: createMouseMovement('i', null, -DISTANCES.short),
-};
-const UP_LEFT = {
-  fast: createMouseMovement('u', -DISTANCES.long, -DISTANCES.long),
-  med: createMouseMovement('u', -DISTANCES.med, -DISTANCES.med),
-  slow: createMouseMovement('u', -DISTANCES.short, -DISTANCES.short),
-};
-const UP_RIGHT = {
-  fast: createMouseMovement('o', DISTANCES.long, -DISTANCES.long),
-  med: createMouseMovement('o', DISTANCES.med, -DISTANCES.med),
-  slow: createMouseMovement('o', DISTANCES.short, -DISTANCES.short),
-};
-const DOWN = {
-  fast: createMouseMovement('comma', null, DISTANCES.long),
-  med: createMouseMovement('comma', null, DISTANCES.med),
-  slow: createMouseMovement('comma', null, DISTANCES.short),
-};
-const DOWN_LEFT = {
-  fast: createMouseMovement('m', -DISTANCES.long, DISTANCES.long),
-  med: createMouseMovement('m', -DISTANCES.med, DISTANCES.med),
-  slow: createMouseMovement('m', -DISTANCES.short, DISTANCES.short),
-};
-const DOWN_RIGHT = {
-  fast: createMouseMovement('period', DISTANCES.long, DISTANCES.long),
-  med: createMouseMovement('period', DISTANCES.med, DISTANCES.med),
-  slow: createMouseMovement('period', DISTANCES.short, DISTANCES.short),
-};
-const LEFT = {
-  fast: createMouseMovement('j', -DISTANCES.long),
-  med: createMouseMovement('j', -DISTANCES.med),
-  slow: createMouseMovement('j', -DISTANCES.short),
-};
-const RIGHT = {
-  fast: createMouseMovement('l', DISTANCES.long),
-  med: createMouseMovement('l', DISTANCES.med),
-  slow: createMouseMovement('l', DISTANCES.short),
-};
-
 export const mouse = [
-  UP.slow,
-  UP_LEFT.slow,
-  UP_RIGHT.slow,
-  DOWN.slow,
-  DOWN_LEFT.slow,
-  DOWN_RIGHT.slow,
-  LEFT.slow,
-  RIGHT.slow,
+  makeMouseKey('u', DIRECTIONS['↖'], DISTANCES.short),
+  makeMouseKey('i', DIRECTIONS['↑'], DISTANCES.short),
+  makeMouseKey('o', DIRECTIONS['↗'], DISTANCES.short),
+  makeMouseKey('j', DIRECTIONS['←'], DISTANCES.short),
+  makeMouseKey('l', DIRECTIONS['→'], DISTANCES.short),
+  makeMouseKey('m', DIRECTIONS['↙'], DISTANCES.short),
+  makeMouseKey('comma', DIRECTIONS['↓'], DISTANCES.short),
+  makeMouseKey('period', DIRECTIONS['↘'], DISTANCES.short),
 
   // this is the main button, it's switched in my mac settings
   map('k').toPointingButton('button2'),
@@ -116,14 +94,14 @@ export const mouse = [
   // this is "right-click"
   map('k', ['left_control']).toPointingButton('button1'),
 
-  map('left_shift').toMouseKey({
+  map('f').toMouseKey({
     speed_multiplier: 3.5,
   }),
-  map('left_option').toMouseKey({
+  map('d').toMouseKey({
     speed_multiplier: 0.5,
   }),
-  createScroll('y', 32), // left
-  createScroll('h', -32), // right
-  createScroll('p', null, -32), // up
-  createScroll('semicolon', null, 32), // down
+  makeMouseScroll('y', 32), // left
+  makeMouseScroll('h', -32), // right
+  makeMouseScroll('p', null, -32), // up
+  makeMouseScroll('semicolon', null, 32), // down
 ].flat();
